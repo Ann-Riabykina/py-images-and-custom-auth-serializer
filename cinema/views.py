5981518da7246afedd84a391c9061baf5cd9ca6e
Ann Riabykina <anny.r20.1994@gmail.com>
2026-01-06 20:49:41 +0200

    Fix models.py and views.py

--- cinema/views.py
+++ cinema/views.py
@@ -121,7 +121,7 @@ class MovieViewSet(
         parser_classes=[MultiPartParser, FormParser],
         url_path="upload-image"
     )
-    def upload_image(self, request):
+    def upload_image(self, request, pk=None):
         movie = self.get_object()
         serializer = MovieImageSerializer(
             movie, data=request.data, partial=True
