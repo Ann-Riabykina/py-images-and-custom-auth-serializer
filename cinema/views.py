fc99be693719c51964f97b27e724204f539eec5a
Ann Riabykina <anny.r20.1994@gmail.com>
2026-01-07 23:58:37 +0200

    Some simplification

--- cinema/views.py
+++ cinema/views.py
@@ -126,7 +126,7 @@ class MovieViewSet(
         url_path="upload-image"
     )
     def upload_image(self, request, pk=None):
-        movie = self.get_object() if pk is None else Movie.objects.get(pk=pk)
+        movie = self.get_object()
 
         serializer = MovieImageSerializer(
             movie, data=request.data, partial=True
