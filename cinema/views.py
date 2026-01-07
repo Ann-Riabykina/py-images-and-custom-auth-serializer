b9269378b9bc961b9c403a61087f713eb9dc73ec
Ann Riabykina <anny.r20.1994@gmail.com>
2026-01-07 22:53:37 +0200

    Fix flake8

--- cinema/views.py
+++ cinema/views.py
@@ -136,7 +136,7 @@ class MovieViewSet(
             return response.Response(
                 serializer.data, status=status.HTTP_200_OK
             )
-        return response.Response(serializer.errors, 
+        return response.Response(serializer.errors,
                                  status=status.HTTP_400_BAD_REQUEST)
 
 
